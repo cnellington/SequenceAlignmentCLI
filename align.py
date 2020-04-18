e9d62d0b30d272b98ef0c956b9ec9ce7b81e2370
cnellington <caleb.n.ellington@gmail.com>
2020-04-18 12:57:24 -0700

    initial commit

--- align.py
+++ align.py
@@ -1,50 +1,79 @@
 """
 Caleb Ellington
-CSE 427: Computational Biology
 01/25/2020
-
-Usage: align.py <sequence1.file> <sequence2.file>
-
 """
 
-import sys
 import argparse
-from os.path import isdir, isfile
+from os.path import isfile
 from model import Alignment
 
-
+"""
+Usage: align.py <sequence1.file> <sequence2.file> [-p] [-v] [-s]
+"""
 def main():
     parser = argparse.ArgumentParser(description="Protein/DNA alignment")
 
     parser.add_argument(
-        "file1",
+        "seqfile1",
         action="store",
         help="Location of the first sequence file to align",
     )
 
     parser.add_argument(
-        "file2",
+        "seqfile2",
         action="store",
         help="Location of the second sequence file to align",
     )
 
     parser.add_argument(
-        "--protein",
+        "--verbose",
+        "-v",
+        action="store_true",
+        default=False,
+        help="Display score and path matrices with resulting alignment",
+    )
+
+    parser.add_argument(
+        "--p-value",
         "-p",
         action="store_true",
         default=False,
-        help="Protein file flag",
+        help="Return alignment with p-value",
+    )
+
+    parser.add_argument(
+        "--smith-waterman",
+        "-s",
+        action="store_true",
+        default=False,
+        help="Use Smith-Waterman local alignment instead of Needleman-Wunsch global alignment",
     )
 
     args = parser.parse_args()
-    if not (isfile(args.file1) and isfile(args.file2)):
+    if not (isfile(args.seqfile1) and isfile(args.seqfile2)):
         print("Invalid sequence path")
 
-    alignment_model = Alignment(args.file1, args.file2, args.protein)
+    alignment = Alignment(args.seqfile1, args.seqfile2, gap_cost=-8)
+    if args.smith_waterman:
+        alignment.run_smith_waterman()
+        print(alignment.get_local_alignment())
+        print(f"score: {alignment.get_sw_score()}")
+    else:
+        alignment.run_needleman_wunsch()
+        print(alignment.get_alignment())
+        print(f"score: {alignment.get_nw_score()}")
 
+    if args.p_value:
+        n = 1e3
+        print(f"P-value: {alignment.get_p_value(n=n)} (N={n})")
 
+    if args.verbose:
+        print("\nScore matrix (using BLOSUM62): ")
+        print(alignment.get_score_matrix())
+        print("\nPath traceback matrix [[0,2],[1,X]]: ")
+        print(alignment.get_path_matrix())
+        print()
 
 
 if __name__ == '__main__':
     main()
-
